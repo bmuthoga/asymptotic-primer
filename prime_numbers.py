504c5f58dc04505fe73daa851fa32fbc1e3bc78d
bmuthoga <bmuthoga@gmail.com>
2016-11-30 23:48:17 +0300

    Add files via upload
    Made function to be more readable.

--- prime_numbers.py
+++ prime_numbers.py
@@ -2,9 +2,9 @@ Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:24:40) [MSC v.1500 64 bit (
 Type "copyright", "credits" or "license()" for more information.
 >>> # Function to generate prime numbers from 0 to n.
 >>> 
->>> def func(num):
+>>> def prime_numbers(number):
 	list=[]
-	for i in range(2,num+1):
+	for i in range(2,number+1):
 		if i>1:
 			for x in range(2,i):
 				if(i%x)==0:
