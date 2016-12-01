e6dda9bdfcfd35ad4df7b06aae536d60983544db
bmuthoga <bmuthoga@gmail.com>
2016-12-01 10:55:36 +0300

    Add files via upload
    Completed.

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,9 +1,11 @@
 def prime_numbers(number):
 	list=[]
 	if number < 0:
-        return "Only positive numbers allowed."
-    if type(number) != int:
-        return "Only integers allowed."
+		return "Only positive numbers allowed."
+	if number < 2:
+		return "No prime number existing for given input."
+	if type(number) != int:
+		return "Only integers allowed."
 	for i in range(2,number+1):
 		if i>1:
 			for x in range(2,i):
--- prime_numbers_test.py
+++ prime_numbers_test.py
@@ -11,8 +11,36 @@ class TestPrimeGenerator(unittest.TestCase):
 
 	# Test if function returns error if number less than 2 entered.
 	def test_less_than_two(self):
-		self.assertEqual(prime_numbers.prime_numbers(1))
+		self.assertEqual(prime_numbers.prime_numbers(1), "No prime number existing for given input.")
 
 	# Test if function returns error if wrong type passed to function.
 	def test_invalid_type(self):
-		self.assertEqual(prime_numbers.prime_numbers("String"))
+		self.assertEqual(prime_numbers.prime_numbers("String"), "Only integers allowed.")
+
+	# Test if only positive numbers allowed.
+	def test_only_positive(self):
+		self.assertEqual(prime_numbers.prime_numbers(-1), "Only positive numbers allowed.")
+	
+	# Test for empty parameters.
+	def test_empty_parameter(self):
+		self.assertEqual(prime_numbers.prime_numbers(), "No number entered.")
+
+	# Test for 100.
+	def test_one_hundred(self):
+		self.assertEqual(prime_numbers.prime_numbers(100), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+
+	# Test for 5.
+	def test_five(self):
+		self.assertEqual(prime_numbers.prime_numbers(5), [2, 3, 5])
+
+	# Test for 50.
+	def test_fifty(self):
+		self.assertEqual(prime_numbers.prime_numbers(50), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47])
+
+	# Test for 2.
+	def test_two(self):
+		self.assertEqual(prime_numbers.prime_numbers(2), [2])
+
+	# Test for 1.
+	def test_one(self):
+		self.assertEqual(prime_numbers.prime_numbers(1), "No prime number existing for given input.")
